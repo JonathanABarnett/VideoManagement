ffe9bb45b5c5f8edd9bb5c3e9867ee8294da3700
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-01-07 20:50:30 -0600

    Update from Bootstrap 3 to Bootstrap 4 and other Nuget Packages

---
+++ VideoManagement/VideoManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoManagement.Startup))]
+namespace VideoManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
