8df0eb0f3e9e0dac4fd2462bd13713f0009f8d9b
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-01-07 21:06:44 -0600

    Downgrade to Bootstrap 3 and add Movie Model and Controller

---
+++ VideoManagement/VideoManagement/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoManagement.Models;
+
+namespace VideoManagement.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie {Name = "Lord of the Rings"};
+
+            return View(movie);
+        }
+    }
+}
