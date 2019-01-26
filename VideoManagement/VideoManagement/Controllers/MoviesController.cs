17db7528c23ad1e1d196875479912b8d01836b9f
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-01-26 14:17:22 -0600

    Add Genre and Update Movie

--- VideoManagement/VideoManagement/Controllers/MoviesController.cs
+++ VideoManagement/VideoManagement/Controllers/MoviesController.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VideoManagement.Models;
-using VideoManagement.ViewModels;
 
 namespace VideoManagement.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
 
             return View(movie);
         }
 
-        public IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Lord of the Rings"},
-                new Movie { Id = 2, Name = "Harry Potter"},
-                new Movie { Id = 3, Name = "Planet of the Apes"}
-            };
-        }
 
     }
 }
--- VideoManagement/VideoManagement/Models/Movie.cs
+++ VideoManagement/VideoManagement/Models/Movie.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace VideoManagement.Models
@@ -8,6 +10,19 @@ namespace VideoManagement.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int GenreId { get; set; }
     }
 }
